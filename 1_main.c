31b53b5ee1dc6854756a29f3c7aaf3db319010cc
yxx142 <3337641508@qq.com>
2025-10-25 14:45:02 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,12 +2,10 @@
 //3337641508@qq.com
 //杨曦
 #include <stdio.h>
-
 int main() {
     int score;
     printf("请输入成绩(0-100): ");
     scanf("%d", &score);
-    
     if (score >= 90 && score <= 100) {
         printf("A\n");
     } else if (score >= 80) {
@@ -21,6 +19,5 @@ int main() {
     } else {
         printf("输入错误\n");
     }
-    
     return 0;
 }
