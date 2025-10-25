97d5dd0eecef167959f906b668a125b9905595ff
yxx142 <3337641508@qq.com>
2025-10-25 14:02:26 +0800

    提交2

--- 1_main.c
+++ 1_main.c
@@ -2,26 +2,36 @@
 //3337641508@qq.com
 //杨曦
 #include <stdio.h>
-
-int main() {
-    int score;
-    scanf("%d", &score);
-    
-    // 判断成绩等级
-    if (score >= 90 && score <= 100) {
-        printf("A\n");
-    } else if (score >= 80 && score <= 89) {
-        printf("B\n");
-    } else if (score >= 70 && score <= 79) {
-        printf("C\n");
-    } else if (score >= 60 && score <= 69) {
-        printf("D\n");
-    } else if (score >= 0 && score < 60) {
-        printf("E\n");
-    } else {
-        // 处理无效输入（超出0-100范围的情况）
-        printf("Invalid score\n");
-    }
-    
-    return 0;
-}
+ int main() {
+     int a, b;
+     char op;
+     // 提示输入格式
+     printf("请输入两个整数和运算符（格式：a b 运算符）：");
+     // 读取输入的两个整数和运算符
+     scanf("%d %d %c", &a, &b, &op);
+     
+     // 根据运算符计算并输出结果
+     switch(op) {
+         case '+':
+             printf("%d\n", a + b);
+             break;
+         case '-':
+             printf("%d\n", a - b);
+             break;
+         case '*':
+             printf("%d\n", a * b);
+             break;
+         case '/':
+             // 判断除数是否为0
+             if (b != 0) {
+                 printf("%d\n", a / b);
+             } else {
+                 printf("除数不能为0\n");
+             }
+             break;
+         default:
+             printf("运算符无效\n");
+     }
+     
+     return 0;
+ }
