b11d2e97e2a3abdc2349545e1b281d83f97c2462
yxx142 <3337641508@qq.com>
2025-10-25 15:14:50 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -2,22 +2,19 @@
 //3337641508@qq.com
 //杨曦
 #include <stdio.h>
-int main() {
-    int score;
-    printf("请输入成绩(0-100): ");
-    scanf("%d", &score);
-    if (score >= 90 && score <= 100) {
-        printf("A\n");
-    } else if (score >= 80) {
-        printf("B\n");
-    } else if (score >= 70) {
-        printf("C\n");
-    } else if (score >= 60) {
-        printf("D\n");
-    } else if (score >= 0) {
-        printf("E\n");
-    } else {
-        printf("输入错误\n");
-    } 
-    return 0;
-}
+ int main() {
+     int score;
+     scanf("%d", &score);
+     if (score >= 90 && score <= 100) {
+         printf("A\n");
+     } else if (score >= 80 && score < 90) {
+         printf("B\n");
+     } else if (score >= 70 && score < 80) {
+         printf("C\n");
+     } else if (score >= 60 && score < 70) {
+         printf("D\n");
+     } else if (score >= 0 && score < 60) {
+         printf("E\n");
+     }
+     return 0;
+ }
