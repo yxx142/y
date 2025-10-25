282219049a35e16dd1dc90d872c99a169d968092
yxx142 <3337641508@qq.com>
2025-10-25 13:15:40 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,15 +1,17 @@
 //202510306220
 //3337641508@qq.com
 //杨曦
-#include <stdio.h>
+# 输入成绩并转换为整数
+score = int(input("请输入学生成绩（0-100）："))
 
-int main() {
-    float celsius, fahrenheit;
-    // 获取用户输入的摄氏温度
-    scanf("%f", &celsius);
-    // 进行温度转换
-    fahrenheit = celsius * 9 / 5 + 32;
-    // 保留1位小数输出华氏温度
-    printf("%.1f\n", fahrenheit);
-    return 0;
-}
+# 判断等级
+if score >= 90 and score <= 100:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("E")
