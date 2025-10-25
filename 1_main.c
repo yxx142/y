b8a48b2ad03a7ff7c9f9e7c5759d189ce99635c6
yxx142 <3337641508@qq.com>
2025-10-25 14:29:18 +0800

    提交2

--- 1_main.c
+++ 1_main.c
@@ -5,22 +5,21 @@
 
 int main() {
     int score;
-    
     printf("请输入成绩(0-100): ");
     scanf("%d", &score);
     
     if (score >= 90 && score <= 100) {
         printf("A\n");
-    } else if (score >= 80 && score <= 89) {
+    } else if (score >= 80) {
         printf("B\n");
-    } else if (score >= 70 && score <= 79) {
+    } else if (score >= 70) {
         printf("C\n");
-    } else if (score >= 60 && score <= 69) {
+    } else if (score >= 60) {
         printf("D\n");
-    } else if (score >= 0 && score < 60) {
+    } else if (score >= 0) {
         printf("E\n");
     } else {
-        printf("输入成绩无效！\n");
+        printf("输入错误\n");
     }
     
     return 0;
