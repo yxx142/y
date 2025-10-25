64100adcd4e52e2fd2590e8fa22f873b34df14e4
yxx142 <3337641508@qq.com>
2025-10-25 13:08:08 +0800

    提交1

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,15 @@
-//
-//
-//
+//202510306220
+//3337641508@qq.com
+//杨曦
 #include <stdio.h>
-int main()
-{
-    printf("Hello World");
+
+int main() {
+    float celsius, fahrenheit;
+    // 获取用户输入的摄氏温度
+    scanf("%f", &celsius);
+    // 进行温度转换
+    fahrenheit = celsius * 9 / 5 + 32;
+    // 保留1位小数输出华氏温度
+    printf("%.1f\n", fahrenheit);
     return 0;
 }
