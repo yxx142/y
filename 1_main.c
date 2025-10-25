3c0d6ea65d0ba05248b1316623d0f3f3cb1dab84
yxx142 <3337641508@qq.com>
2025-10-25 15:14:09 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,22 +2,13 @@
 //3337641508@qq.com
 //杨曦
 #include <stdio.h>
-int main() {
-    int score;
-    printf("请输入成绩(0-100): ");
-    scanf("%d", &score);
-    if (score >= 90 && score <= 100) {
-        printf("A\n");
-    } else if (score >= 80) {
-        printf("B\n");
-    } else if (score >= 70) {
-        printf("C\n");
-    } else if (score >= 60) {
-        printf("D\n");
-    } else if (score >= 0) {
-        printf("E\n");
-    } else {
-        printf("输入错误\n");
-    }
-    return 0;
-}
+ int main() {
+     int a, b, c;
+     scanf("%d %d %d", &a, &b, &c);
+     if (a + b > c && a + c > b && b + c > a) {
+         printf("可以组成三角形\n");
+     } else {
+         printf("不能组成三角形\n");
+     }
+     return 0;
+ }
