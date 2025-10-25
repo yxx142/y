16eb7d0b3085c3d41270479c59631f12ee7c5c96
yxx142 <3337641508@qq.com>
2025-10-25 15:21:09 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,14 +1,11 @@
 //202510306220
 //3337641508@qq.com
 //杨曦
-#include <stdio.h>
- int main() {
-     int a, b, c;
-     scanf("%d %d %d", &a, &b, &c);
-     if (a + b > c && a + c > b && b + c > a) {
-         printf("可以组成三角形\n");
-     } else {
-         printf("不能组成三角形\n");
-     }
-     return 0;
- }
+#include<stdio.h>
+int main(){
+    float celsius;
+    scanf("%f",&celsius);
+    float fahrenheit = celsius * 9/5 + 32;
+    printf("%.1f\n",fahrenheit);
+    return 0;
+}
