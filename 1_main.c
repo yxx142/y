d0cb2a9ca4c4b66854425d6d51a438e92de898bf
yxx142 <3337641508@qq.com>
2025-10-25 14:05:30 +0800

    提交2

--- 1_main.c
+++ 1_main.c
@@ -2,36 +2,32 @@
 //3337641508@qq.com
 //杨曦
 #include <stdio.h>
- int main() {
-     int a, b;
-     char op;
-     // 提示输入格式
-     printf("请输入两个整数和运算符（格式：a b 运算符）：");
-     // 读取输入的两个整数和运算符
-     scanf("%d %d %c", &a, &b, &op);
-     
-     // 根据运算符计算并输出结果
-     switch(op) {
-         case '+':
-             printf("%d\n", a + b);
-             break;
-         case '-':
-             printf("%d\n", a - b);
-             break;
-         case '*':
-             printf("%d\n", a * b);
-             break;
-         case '/':
-             // 判断除数是否为0
-             if (b != 0) {
-                 printf("%d\n", a / b);
-             } else {
-                 printf("除数不能为0\n");
-             }
-             break;
-         default:
-             printf("运算符无效\n");
-     }
-     
-     return 0;
- }
+
+int main() {
+    int score;
+    
+    // 获取用户输入
+    printf("请输入学生的成绩（0-100）：");
+    scanf("%d", &score);
+    
+    // 验证输入范围
+    if (score < 0 || score > 100) {
+        printf("输入错误！成绩应该在0-100之间。\n");
+        return 1;
+    }
+    
+    // 判断成绩等级（从高分到低分判断）
+    if (score >= 90) {
+        printf("A\n");
+    } else if (score >= 80) {
+        printf("B\n");
+    } else if (score >= 70) {
+        printf("C\n");
+    } else if (score >= 60) {
+        printf("D\n");
+    } else {
+        printf("E\n");
+    }
+    
+    return 0;
+}
