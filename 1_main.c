96306772de1aedef2e65579ac3a41d6a3bade737
yxx142 <3337641508@qq.com>
2025-11-08 15:58:51 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -4,17 +4,23 @@
 #include <stdio.h>
 
 int main() {
-    int matrix[3][3];
+    int matrix[3][3], transpose[3][3];
     // 输入矩阵
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             scanf("%d", &matrix[i][j]);
         }
     }
-    // 输出矩阵
+    // 计算转置矩阵
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            printf("%d ", matrix[i][j]);
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+    // 输出转置矩阵
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", transpose[i][j]);
         }
         printf("\n");
     }
