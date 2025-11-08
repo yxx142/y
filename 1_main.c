d11d03fe275b40fabedb1a179021ee995f161986
yxx142 <3337641508@qq.com>
2025-11-08 15:59:45 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -3,26 +3,21 @@
 //杨曦
 #include <stdio.h>
 
+// 计算a的b次幂
+int power(int a, int b) {
+    int result = 1;
+    for (int i = 0; i < b; i++) {
+        result *= a;
+    }
+    return result;
+}
+
 int main() {
-    int nums[10];
-    // 输入10个数
-    for (int i = 0; i < 10; i++) {
-        scanf("%d", &nums[i]);
+    int sum = 0;
+    // 计算1²+2²+3²+4²+5²
+    for (int i = 1; i <= 5; i++) {
+        sum += power(i, 2);
     }
-    // 冒泡排序
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9 - i; j++) {
-            if (nums[j] > nums[j + 1]) {
-                int temp = nums[j];
-                nums[j] = nums[j + 1];
-                nums[j + 1] = temp;
-            }
-        }
-    }
-    // 输出排序结果
-    for (int i = 0; i < 10; i++) {
-        printf("%d ", nums[i]);
-    }
-    printf("\n");
+    printf("%d\n", sum);
     return 0;
 }
